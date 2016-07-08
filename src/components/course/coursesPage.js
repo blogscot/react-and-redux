af096baed714202909b0ac402b88a40460e46144
blogscot <iain.diamond@gmail.com>
2016-07-08 11:36:33 +0100

    Course Author field

--- src/components/course/coursesPage.js
+++ src/components/course/coursesPage.js
@@ -7,10 +7,11 @@ class CoursesPage extends React.Component {
     super(props, context)
 
     this.state = {
-      course: { title: "" }
+      course: { title: "", author: "" }
     }
 
     this.onTitleChange = this.onTitleChange.bind(this)
+    this.onAuthorChange = this.onAuthorChange.bind(this)
     this.onClickSave = this.onClickSave.bind(this)
   }
 
@@ -20,12 +21,18 @@ class CoursesPage extends React.Component {
     this.setState({course: course})
   }
 
+  onAuthorChange(event) {
+    const course = this.state.course
+    course.author = event.target.value
+    this.setState({course: course})
+  }
+
   onClickSave() {
-    this.props.dispatch(courseActions.createCourse(this.state.course))
+    this.props.createCourse(this.state.course)
   }
 
   courseRow(course, index) {
-    return <div key={index}>{course.title}</div>
+    return <div key={index}>{course.title} by <em>{course.author}</em></div>
   }
 
   render() {
@@ -34,10 +41,20 @@ class CoursesPage extends React.Component {
         <h1>Courses</h1>
         {this.props.courses.map(this.courseRow)}
         <h2>Add Course</h2>
+
+        <label>Title:</label>&nbsp;
         <input
           type="text"
           onChange={this.onTitleChange}
           value={this.state.course.title} />
+        <br />
+
+        <label>Author:</label>&nbsp;
+        <input
+          type="text"
+          onChange={this.onAuthorChange}
+          value={this.state.course.author} />
+          <br />
 
         <input
           type="submit"
@@ -49,8 +66,8 @@ class CoursesPage extends React.Component {
 }
 
 CoursesPage.propTypes = {
-  dispatch: PropTypes.func.isRequired,
-  courses: PropTypes.array.isRequired
+  courses: PropTypes.array.isRequired,
+  createCourse: PropTypes.func.isRequired
 }
 
 function mapStateToProps(state, ownProps) {
@@ -59,4 +76,10 @@ function mapStateToProps(state, ownProps) {
   }
 }
 
-export default connect(mapStateToProps)(CoursesPage)
+function mapDispatchToProps(dispatch) {
+  return {
+    createCourse: course => dispatch(courseActions.createCourse(course))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage)
