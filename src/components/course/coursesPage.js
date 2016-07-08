7e2e7eaa45196fb201c1a55f6779edf40f106df7
blogscot <iain.diamond@gmail.com>
2016-07-08 14:31:10 +0100

    Mock Courses Api

--- src/components/course/coursesPage.js
+++ src/components/course/coursesPage.js
@@ -6,30 +6,6 @@ import * as courseActions from '../../actions/courseActions'
 class CoursesPage extends React.Component {
   constructor(props, context) {
     super(props, context)
-
-    this.state = {
-      course: { title: "", author: "" }
-    }
-
-    this.onTitleChange = this.onTitleChange.bind(this)
-    this.onAuthorChange = this.onAuthorChange.bind(this)
-    this.onClickSave = this.onClickSave.bind(this)
-  }
-
-  onTitleChange(event) {
-    const course = this.state.course
-    course.title = event.target.value
-    this.setState({course: course})
-  }
-
-  onAuthorChange(event) {
-    const course = this.state.course
-    course.author = event.target.value
-    this.setState({course: course})
-  }
-
-  onClickSave() {
-    this.props.actions.createCourse(this.state.course)
   }
 
   courseRow(course, index) {
@@ -41,26 +17,6 @@ class CoursesPage extends React.Component {
       <div>
         <h1>Courses</h1>
         {this.props.courses.map(this.courseRow)}
-        <h2>Add Course</h2>
-
-        <label>Title:</label>&nbsp;
-        <input
-          type="text"
-          onChange={this.onTitleChange}
-          value={this.state.course.title} />
-        <br />
-
-        <label>Author:</label>&nbsp;
-        <input
-          type="text"
-          onChange={this.onAuthorChange}
-          value={this.state.course.author} />
-          <br />
-
-        <input
-          type="submit"
-          value="Save"
-          onClick={this.onClickSave} />
       </div>
     )
   }
