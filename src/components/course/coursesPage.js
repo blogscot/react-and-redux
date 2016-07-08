5a89bee2b3aab5dc29c92c2f5a2e20da1171a2cf
blogscot <iain.diamond@gmail.com>
2016-07-08 16:14:56 +0100

    Course List

--- src/components/course/coursesPage.js
+++ src/components/course/coursesPage.js
@@ -1,7 +1,9 @@
 import React, {PropTypes} from 'react'
 import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
+
 import * as courseActions from '../../actions/courseActions'
+import CourseList from './courseList'
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
@@ -13,10 +15,11 @@ class CoursesPage extends React.Component {
   }
 
   render() {
+    const {courses} = this.props
     return (
       <div>
         <h1>Courses</h1>
-        {this.props.courses.map(this.courseRow)}
+        <CourseList courses={courses}/>
       </div>
     )
   }
